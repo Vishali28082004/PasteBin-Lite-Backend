445d47ef8ec31b3c098d6807ab2ee9237b011363
Kannan <174104295+Vishali28082004@users.noreply.github.com>
2025-12-31 16:20:06 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: config.FRONTEND_URL || "http://localhost:3000",
+    origin: "https://pastebin-frontend-mauve.vercel.app/",
     credentials: true,
   })
 );
